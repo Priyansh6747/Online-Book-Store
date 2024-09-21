70b8820ec3541a0b6eb3f056555e847268e32d3a
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-09-21 11:06:53 +0530

    Add sign-up functionality
    Implemented a new SignUp page with HTML, CSS, and JavaScript for form validation and submission. The form checks for matching passwords and sends user data to the server upon successful validation.

---
+++ SignUp.js
@@ -0,0 +1,36 @@
+﻿
+//validate form
+function ValidateForm()
+{
+    //variables with DOM
+    const Name = document.getElementById("Name").value;
+    const Username = document.getElementById("Username").value;
+    const Email = document.getElementById("email").value;
+    const Password = document.getElementById("Pass").value;
+    const CPassword = document.getElementById("CPass").value;
+    //variables to object
+    const userdata = {Name: Name, Username: Username, Email: Email, Password: Password};
+    if(Password !== CPassword) {
+        alert("Passwords don't match");
+        return false;
+    }
+    fetch("http://localhost/OnlineBookStore/CreateUser.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",},
+        body: JSON.stringify({userdata}),
+    })
+        .then(response => {
+            if(!response.ok)
+                console.log(response.error);
+            else
+                return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            window.location.href = "Login.html";
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
