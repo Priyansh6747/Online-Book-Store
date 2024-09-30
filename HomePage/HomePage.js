8d0b5a64174f3113d710b712b2665786af8faf9c
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-09-30 23:17:38 +0530

    SeeAll button for BestSeller Section AddToCart button functionality, rating, auther's name  remaining

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -10,30 +10,60 @@ SignInBtn.addEventListener("click", () =>{
 const BSeeAllBtn =document.getElementById("BestSellerSeeAll");
 BSeeAllBtn.addEventListener("click", () =>{
     event.preventDefault();
-    if(BSeeAllBtn.value === "See all"){
+    if(BSeeAllBtn.textContent === "SeeAll"){
         //Getting variables
-        let mainContainer = document.querySelector("#BestSellerSeeAll");
+        let mainContainer = document.querySelector("#BestSellerContainer");
         //fetching JSON
         fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
             .then(res => {
-                if(!res.ok){throw new Error('HTTP error! Status: ' + res.status)}
+                if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
                 else
                     return res.json();
             })
             .then(data => {
                 console.log(data);
-                let template;
                 data.forEach((item) => {
-                    template = mainContainer.querySelector(".template");
-                    template.style.display = "block";
-                    template.classList.add("removable");
-                    template.classList.remove("template");
-                    template.querySelector("img").src = item.URL;
-                    template.querySelector("img").alt = item.Name;
-                    template.querySelector(".book-details").querySelector("#BookTitle").textContent = item.Name;
-                    template.querySelector(".book-details").querySelector("#BookPrice").textContent = item.Price;
+                    //BookItem div
+                    let Book_item = document.createElement("div");
+                    Book_item.classList.add("book_item");
+                    //Image
+                    let img = document.createElement("img");
+                    img.classList.add("book-image");
+                    img.src = item.URL;
+                    Book_item.appendChild(img);
+
+                    //Book Details div
+                    let book_details = document.createElement("div");
+                    book_details.classList.add("book-details");
+                    let Name = document.createElement("p");
+                    Name.textContent = item.Name;
+                    Name.classList.add("book-title");
+                    book_details.appendChild(Name);
+
+                    let price = document.createElement("p");
+                    price.textContent = item.Price;
+                    price.classList.add("price");
+                    book_details.appendChild(price);
+
+                    Book_item.appendChild(book_details);
+
+                    let auther = document.createElement("p");
+                    auther.textContent = item.name;
+                    auther.classList.add("auther");
+                    Book_item.appendChild(auther);
+
+                    let AddToCartBtn = document.createElement("button");
+                    AddToCartBtn.textContent = "Add To Cart";
+                    AddToCartBtn.classList.add("add-book-button");
+                    let CartIcon = document.createElement("i");
+                    CartIcon.classList.add("fa-solid,fa-cart-plus");
+                    AddToCartBtn.appendChild(CartIcon);
+                    Book_item.appendChild(AddToCartBtn);
+
+                    Book_item.classList.add("RemoveOnSeeLess");
+                    mainContainer.appendChild(Book_item);
                 })
-                mainContainer.appendChild(template);
+                BSeeAllBtn.textContent = "See Less";
             })
             .catch(error => {
                 console.log(error);
@@ -41,7 +71,10 @@ BSeeAllBtn.addEventListener("click", () =>{
     }
     else
     {
-        //remove the extra books
+        let remove = document.querySelectorAll(".RemoveOnSeeLess");
+        remove.forEach((item) => {item.remove();});
+        BSeeAllBtn.textContent = "SeeAll";
+        console.log("remove");
     }
 
 })
