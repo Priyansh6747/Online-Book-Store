1c5dc757087536a754158e7fe0cf03c06dd0b01c
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-10-03 17:04:35 +0530

    Fix some bugs with login page, now working perfectly

--- LoginPage/Login.js
+++ LoginPage/Login.js
@@ -1,22 +1,29 @@
-﻿
+﻿const form = document.getElementById("Form");
+form.addEventListener('submit', (e) => {e.preventDefault()});
 const btn = document.getElementById("Submit");
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     const UserNameInput = document.getElementById("Username").value;
     const PasswordInput = document.getElementById("Pass").value;
+    const User = {Username : UserNameInput, Password: PasswordInput};
     fetch("http://localhost/OnlineBookStore/UserInfo.php", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(UserNameInput)})
+        body: JSON.stringify(User),
+    })
         .then(response => {
             if(!response.ok)
                 throw new Error(`HTTP error! Status: ${response.status}`);
             else return response.json();
         })
         .then(data => {
+            console.log(data);
             if(data.Password === PasswordInput) window.location.href = "../HomePage/HomePage.html";
             else alert("Username or Password incorrect.");
         })
+        .catch(error => {
+            console.log(error);
+        });
 })
