572f1e9cd003a994647bbfb47a7c3d2f6a6091ad
sahil1607cms <sahil16072004@gmail.com>
2024-10-26 15:28:28 +0530

    added comment explaination in login js

--- LoginPage/Login.js
+++ LoginPage/Login.js
@@ -1,28 +1,30 @@
 ﻿const form = document.getElementById("Form");
-form.addEventListener('submit', (e) => {e.preventDefault()});
+form.addEventListener('submit', (e) => { e.preventDefault() });
 const btn = document.getElementById("Submit");
 btn.addEventListener("click", (e) => {
     e.preventDefault();
     const UserNameInput = document.getElementById("Username").value;
     const PasswordInput = document.getElementById("Pass").value;
-    const User = {Username : UserNameInput, Password: PasswordInput};
+    const User = { Username: UserNameInput, Password: PasswordInput };
     fetch("http://localhost/OnlineBookStore/UserInfo.php", {
+        // Sending data to server
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
+        //the body of fetch should be string when using Content-Type: application/json so that the server can understand 
         body: JSON.stringify(User),
     })
         .then(response => {
-            if(!response.ok)
+            if (!response.ok)
                 throw new Error(`HTTP error! Status: ${response.status}`);
             else return response.json();
         })
         .then(data => {
             console.log(data);
-            if(data.Password === PasswordInput) {
+            if (data.Password === PasswordInput) {
                 window.localStorage.setItem("username", UserNameInput);
-                window.localStorage.setItem("isLoggedIn","true");
+                window.localStorage.setItem("isLoggedIn", "true");
                 window.location.href = "../HomePage/HomePage.html";
             }
             else alert("Username or Password incorrect.");
