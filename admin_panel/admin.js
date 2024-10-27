0e7d301a291022272e391ce10c8331af4e989397
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-10-27 15:36:50 +0530

    backend connectivity not working currently

--- admin_panel/admin.js
+++ admin_panel/admin.js
@@ -1,8 +1,6 @@
-const b_form = document.getElementById('bookForm');
-
-b_form.addEventListener('submit', function (e) {
+let SubBtn = document.getElementById("SubmitBTN");
+SubBtn.addEventListener('submit', function (e) {
     e.preventDefault();
-
     const formData = new FormData();
     formData.append('bookName', document.getElementById('bookName').value);
     formData.append('url', document.getElementById('url').value);
@@ -10,17 +8,19 @@ b_form.addEventListener('submit', function (e) {
     formData.append('rating', document.getElementById('rating').value);
     formData.append('authorName', document.getElementById('authorName').value);
     formData.append('authorBio', document.getElementById('authorBio').value);
+    formData.append('Author_URL', document.getElementById('Author_URL').value);
 
     // Send data to PHP server using fetch
-    fetch('add_book.php', {
-        method: "POST",
-        body: formData
+    fetch("http://localhost/ONLINEBOOKSTORE/AddBook.php", {
+        method: "POST",headers: {
+            "Content-Type": "application/json",},
+        body: JSON.stringify(formData),
     })
         .then(response => {
             if (!response.ok) {
-                throw new Error('Failed to add the book');
+                throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return response.text();
+            return response.json();
         })
         .then(result => {
             console.log(result);
