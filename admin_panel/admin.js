232482a4d9c9a6e73de4737f0a2c8ce1499521b4
sahil1607cms <sahil16072004@gmail.com>
2024-10-27 14:07:59 +0530

    added js code to handle book adding

---
+++ admin_panel/admin.js
@@ -0,0 +1,33 @@
+const b_form = document.getElementById('bookForm');
+
+b_form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const formData = new FormData();
+    formData.append('bookName', document.getElementById('bookName').value);
+    formData.append('url', document.getElementById('url').value);
+    formData.append('price', document.getElementById('price').value);
+    formData.append('rating', document.getElementById('rating').value);
+    formData.append('authorName', document.getElementById('authorName').value);
+    formData.append('authorBio', document.getElementById('authorBio').value);
+
+    // Send data to PHP server using fetch
+    fetch('add_book.php', {
+        method: "POST",
+        body: formData
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Failed to add the book');
+            }
+            return response.text();
+        })
+        .then(result => {
+            console.log(result);
+            alert('Book successfully added!');
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Failed to add the book due to some error. Please try again.'); 
+        });
+    })
