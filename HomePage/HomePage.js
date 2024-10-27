6517147747790236d25ccb70ca49036daca1a625
sahil1607cms <sahil16072004@gmail.com>
2024-10-27 13:51:33 +0530

    added admin panel

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -3,28 +3,28 @@
 
 //Sign In redirect
 let IsLoggedIn = false;
-if(localStorage.getItem("isLoggedIn") == "true") {
+if (localStorage.getItem("isLoggedIn") == "true") {
     document.getElementById("UserAccount").style.display = "block";
     document.getElementById("SignIn").style.display = "none";
     localStorage.setItem("isLoggedIn", "true");
     document.getElementById("AccountUserName").innerHTML = localStorage.getItem("username");
 }
 
-function logOut(){
+function logOut() {
     IsLoggedIn = false;
-    localStorage.setItem("isLoggedIn","false");
+    localStorage.setItem("isLoggedIn", "false");
     console.log(localStorage.getItem("isLoggedIn"));
     window.location.reload();
 }
 
-const SignInBtn =document.getElementById("SignIn");
-SignInBtn.addEventListener("click", (e) =>{
+const SignInBtn = document.getElementById("SignIn");
+SignInBtn.addEventListener("click", (e) => {
     window.location.href = "../LoginPage/Login.html";
     e.preventDefault();
 })
 
 //search button
-document.querySelector("#Search").addEventListener("click", (e) =>{
+document.querySelector("#Search").addEventListener("click", (e) => {
     e.preventDefault();
     localStorage.setItem('SearchQ', document.getElementById("search_book").value);
     window.location.href = "../SearchPage/Search.html";
@@ -32,16 +32,16 @@ document.querySelector("#Search").addEventListener("click", (e) =>{
 
 
 //BestSeller
-const BSeeAllBtn =document.getElementById("BestSellerSeeAll");
-BSeeAllBtn.addEventListener("click", (event) =>{
+const BSeeAllBtn = document.getElementById("BestSellerSeeAll");
+BSeeAllBtn.addEventListener("click", (event) => {
     event.preventDefault();
-    if(BSeeAllBtn.textContent === "SeeAll"){
+    if (BSeeAllBtn.textContent === "SeeAll") {
         //Getting variables
         let mainContainer = document.querySelector("#BestSellerContainer");
         //fetching JSON
         fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
             .then(res => {
-                if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+                if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
                 else
                     return res.json();
             })
@@ -66,7 +66,7 @@ BSeeAllBtn.addEventListener("click", (event) =>{
 
                     //price div
                     let price = document.createElement("p");
-                    price.textContent = "$"+item.Price;
+                    price.textContent = "$" + item.Price;
                     price.classList.add("price");
                     book_details.appendChild(price);
                     Book_item.appendChild(book_details);
@@ -80,10 +80,10 @@ BSeeAllBtn.addEventListener("click", (event) =>{
                     //Rating div
                     let rating = document.createElement("div");
                     rating.classList.add("rating");
-                    let i=1;
-                    for(; i<= item.Rating ; i++){
+                    let i = 1;
+                    for (; i <= item.Rating; i++) {
                         let icon = document.createElement("i");
-                        icon.classList.add("fa","fa-star");
+                        icon.classList.add("fa", "fa-star");
                         rating.appendChild(icon);
                     }
                     Book_item.appendChild(rating);
@@ -92,7 +92,7 @@ BSeeAllBtn.addEventListener("click", (event) =>{
                     AddToCartBtn.textContent = "Add";
                     AddToCartBtn.classList.add("add-book-button");
                     let CartIcon = document.createElement("i");
-                    CartIcon.classList.add("fa-solid","fa-cart-plus");
+                    CartIcon.classList.add("fa-solid", "fa-cart-plus");
                     AddToCartBtn.appendChild(CartIcon);
                     Book_item.appendChild(AddToCartBtn);
 
@@ -105,10 +105,9 @@ BSeeAllBtn.addEventListener("click", (event) =>{
                 console.log(error);
             })
     }
-    else
-    {
+    else {
         let remove = document.querySelectorAll(".RemoveOnSeeLess");
-        remove.forEach((item) => {item.remove();});
+        remove.forEach((item) => { item.remove(); });
         BSeeAllBtn.textContent = "SeeAll";
         console.log("remove");
     }
@@ -122,20 +121,20 @@ let Author2Info = document.getElementById("AboutAuthor2");
 let Auth1Img = document.getElementById("Author1Img");
 let Auth2Img = document.getElementById("Author2Img");
 function getRandomElements(arr) {
-    if (arr.length < 2) return [arr[0],arr[0]];
+    if (arr.length < 2) return [arr[0], arr[0]];
     let index1 = Math.floor(Math.random() * arr.length);
     let index2;
     do index2 = Math.floor(Math.random() * arr.length); while (index2 === index1);
     return [arr[index1], arr[index2]];
 }
 fetch("http://localhost/ONLINEBOOKSTORE/GetAuthors.php")
-.then(res =>{
-    if(!res.ok)
-        throw new Error(`HTTP error! Status: ${res.status}`);
-    return res.json();
-})
+    .then(res => {
+        if (!res.ok)
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        return res.json();
+    })
     .then(data => {
-        let random =getRandomElements(data);
+        let random = getRandomElements(data);
         let aut1 = random[0];
         let aut2 = random[1];
         Author1.textContent = aut1.Author_Name;
@@ -145,6 +144,6 @@ fetch("http://localhost/ONLINEBOOKSTORE/GetAuthors.php")
         Author2Info.textContent = aut2.Author_Bio;
         Auth2Img.src = aut2.Image;
     })
-.catch(error => {
-    console.log(error);
-})
+    .catch(error => {
+        console.log(error);
+    })
