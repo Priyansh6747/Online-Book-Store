f493824de90ee4f4759e377ccee5dbad422efe73
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-10-31 13:31:10 +0530

    Added a show more button in after see all A show more button when clicked loads 4 books click again to load 4 more dispersers when all books are loaded

--- HomePage/HomePage.js
+++ HomePage/HomePage.js
@@ -30,86 +30,109 @@ document.querySelector("#Search").addEventListener("click", (e) => {
     window.location.href = "../SearchPage/Search.html";
 })
 
-
+let CurrentDisplayedBook =0;
+//Add 4 books on page awake as non removable
+for(let i = 0; i < 4; i++){
+    LoadBook(CurrentDisplayedBook,"NonRemovableBook");
+    CurrentDisplayedBook++;
+}
 //BestSeller
+function LoadBook(CurrentNoOfBooks,Tag){
+    let mainContainer = document.querySelector("#BestSellerContainer");
+    fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
+        .then(res => {
+            if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+            else
+                return res.json();
+        })
+        .then(data => {
+            if(CurrentNoOfBooks  === data.length) return;
+            if(CurrentNoOfBooks + 1 === data.length) ShowMoreBTN.style.display = "none";
+            let item = data[CurrentNoOfBooks];
+            //BookItem div
+            let Book_item = document.createElement("div");
+            Book_item.classList.add("book_item");
+            //Image
+            let img = document.createElement("img");
+            img.classList.add("book-image");
+            img.src = item.URL;
+            Book_item.appendChild(img);
+
+            //Book Details div
+            let book_details = document.createElement("div");
+            book_details.classList.add("book-details");
+            let Name = document.createElement("p");
+            Name.textContent = item.Name;
+            Name.classList.add("book-title");
+            book_details.appendChild(Name);
+
+            //price div
+            let price = document.createElement("p");
+            price.textContent = "$" + item.Price;
+            price.classList.add("price");
+            book_details.appendChild(price);
+            Book_item.appendChild(book_details);
+
+            //book author para
+            let author = document.createElement("p");
+            author.textContent = "by: " + item.Author_Name;
+            author.classList.add("author");
+            Book_item.appendChild(author);
+
+            //Rating div
+            let rating = document.createElement("div");
+            rating.classList.add("rating");
+            let i = 1;
+            for (; i <= item.Rating; i++) {
+                let icon = document.createElement("i");
+                icon.classList.add("fa", "fa-star");
+                rating.appendChild(icon);
+            }
+            Book_item.appendChild(rating);
+
+            let AddToCartBtn = document.createElement("button");
+            AddToCartBtn.textContent = "Add";
+            AddToCartBtn.classList.add("add-book-button");
+            let CartIcon = document.createElement("i");
+            CartIcon.classList.add("fa-solid", "fa-cart-plus");
+            AddToCartBtn.appendChild(CartIcon);
+            //event listner for add to cart
+            Book_item.appendChild(AddToCartBtn);
+
+            Book_item.classList.add(Tag);
+            mainContainer.appendChild(Book_item);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+const ShowMoreBTN = document.querySelector("#ShowMoreButton");
 const BSeeAllBtn = document.getElementById("BestSellerSeeAll");
 BSeeAllBtn.addEventListener("click", (event) => {
     event.preventDefault();
     if (BSeeAllBtn.textContent === "SeeAll") {
-        //Getting variables
-        let mainContainer = document.querySelector("#BestSellerContainer");
-        //fetching JSON
-        fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
-            .then(res => {
-                if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
-                else
-                    return res.json();
-            })
-            .then(data => {
-                data.forEach((item) => {
-                    //BookItem div
-                    let Book_item = document.createElement("div");
-                    Book_item.classList.add("book_item");
-                    //Image
-                    let img = document.createElement("img");
-                    img.classList.add("book-image");
-                    img.src = item.URL;
-                    Book_item.appendChild(img);
-
-                    //Book Details div
-                    let book_details = document.createElement("div");
-                    book_details.classList.add("book-details");
-                    let Name = document.createElement("p");
-                    Name.textContent = item.Name;
-                    Name.classList.add("book-title");
-                    book_details.appendChild(Name);
-
-                    //price div
-                    let price = document.createElement("p");
-                    price.textContent = "$" + item.Price;
-                    price.classList.add("price");
-                    book_details.appendChild(price);
-                    Book_item.appendChild(book_details);
-
-                    //book author para
-                    let author = document.createElement("p");
-                    author.textContent = "by: " + item.Author_Name;
-                    author.classList.add("author");
-                    Book_item.appendChild(author);
-
-                    //Rating div
-                    let rating = document.createElement("div");
-                    rating.classList.add("rating");
-                    let i = 1;
-                    for (; i <= item.Rating; i++) {
-                        let icon = document.createElement("i");
-                        icon.classList.add("fa", "fa-star");
-                        rating.appendChild(icon);
-                    }
-                    Book_item.appendChild(rating);
-
-                    let AddToCartBtn = document.createElement("button");
-                    AddToCartBtn.textContent = "Add";
-                    AddToCartBtn.classList.add("add-book-button");
-                    let CartIcon = document.createElement("i");
-                    CartIcon.classList.add("fa-solid", "fa-cart-plus");
-                    AddToCartBtn.appendChild(CartIcon);
-                    Book_item.appendChild(AddToCartBtn);
-
-                    Book_item.classList.add("RemoveOnSeeLess");
-                    mainContainer.appendChild(Book_item);
-                })
-                BSeeAllBtn.textContent = "See Less";
-            })
-            .catch(error => {
-                console.log(error);
-            })
+        for(let i = 0; i < 4 ; i++) {
+            LoadBook(CurrentDisplayedBook,"RemoveOnSeeLess");
+            CurrentDisplayedBook++;
+        }
+        ShowMoreBTN.style.display = "block";
+        BSeeAllBtn.textContent = "See Less";
     }
     else {
         let remove = document.querySelectorAll(".RemoveOnSeeLess");
-        remove.forEach((item) => { item.remove(); });
+        remove.forEach((item) => {
+            item.remove();
+            CurrentDisplayedBook--;
+        });
         BSeeAllBtn.textContent = "SeeAll";
-        console.log("remove");
+    }
+})
+
+ShowMoreBTN.addEventListener("click", (event) => {
+    for(let i =0; i < 4; i++){
+        LoadBook(CurrentDisplayedBook,"RemoveOnSeeLess");
+        CurrentDisplayedBook++;
     }
 })
 
