79d7d3e519a49ad1d90dd2e5e72385f39cf24343
Priyansh6747 <154755072+Priyansh6747@users.noreply.github.com>
2024-11-02 13:07:29 +0530

    New Option to manage users through GUI only added a new div(styling included) and done backend functionality needed for it

--- admin_panel/admin.js
+++ admin_panel/admin.js
@@ -58,11 +58,90 @@ function DeleteBook(BookBID) {
         })
 }
 
+function DeleteUser(UID){
+    fetch("http://localhost/ONLINEBOOKSTORE/DeleteUser.php",{
+        method: "POST",headers: { 'Content-Type': 'application/json'},
+        body: JSON.stringify({uid:UID}),
+    })
+        .then(res =>{
+            if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+            else return res.json();
+        })
+        .then(data=>{
+            console.log(data.message);
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        })
+    console.log({UID});
+}
+
+function LoadAllUsers(){
+    let MainContainer = document.querySelector(".UserContainer");
+    fetch("http://localhost/ONLINEBOOKSTORE/GetUsers.php")
+        .then(res =>{
+            if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+            else return res.json();
+        })
+        .then(data =>{
+            data.forEach((item) => {
+                let card = document.createElement("div");
+                card.classList.add("UserCard");
+
+                let icon = document.createElement("i");
+                icon.classList.add("fa","fa-user");
+                icon.style.fontSize = "24px";
+                card.appendChild(icon);
+
+                let UID = document.createElement("p");
+                UID.textContent = item.UID;
+                UID.classList.add("UserText");
+                card.appendChild(UID);
+
+                let Name = document.createElement("p");
+                Name.textContent = item.Name;
+                Name.classList.add("UserText");
+                card.appendChild(Name);
+
+                let UserName = document.createElement("p");
+                UserName.textContent = item.Username;
+                UserName.classList.add("UserText");
+                card.appendChild(UserName);
+
+                let Email = document.createElement("p");
+                Email.textContent = item.Email;
+                Email.classList.add("UserText");
+                card.appendChild(Email);
+
+                let DeleteBTN = document.createElement("Button");
+                DeleteBTN.textContent= "Delete";
+                DeleteBTN.classList.add("DeleteBTN");
+                DeleteBTN.addEventListener("click", function (e) {
+                    DeleteUser(UID.textContent)
+                    DeleteBTN.textContent = "deleting..";
+                    setTimeout(()=>{
+                        window.location.reload();
+                    }, 3500);
+                })
+                card.appendChild(DeleteBTN);
+                card.classList.add("RemovableUsers");
+                MainContainer.appendChild(card);
+            })
+        })
+}
+
 let i=1;
 let SeeAllBtn = document.getElementById("SAAB");
 let BookContainer = document.getElementById("BookContainer");
 SeeAllBtn.addEventListener("click", function (e) {
-    if(i !== 1) return;
+    if(i === 0) {
+        i++;
+        SeeAllBtn.textContent = "SeeAllBooks";
+        let book = document.querySelectorAll(".RemovableBook");
+        book.forEach(item => {item.remove();})
+        return;
+    }
+    SeeAllBtn.textContent = "Hide"
     i--;
     fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
     .then(response => {
@@ -102,6 +181,7 @@ SeeAllBtn.addEventListener("click", function (e) {
                     }, 3500);
                 })
                 card.appendChild(DeleteBTN);
+                card.classList.add("RemovableBook");
                 BookContainer.appendChild(card);
             })
         })
@@ -122,5 +202,19 @@ AddBookBTN.addEventListener("click", function (e) {
         AddBookBTN.textContent = "Add";
         AddBookBTN.classList.remove("DeleteBTN");
         AddBookBTN.classList.add("AddBTN");
+    }
+})
+
+const AddUserBTN = document.getElementById("SAU");
+AddUserBTN.addEventListener("click", function (e) {
+    if(AddUserBTN.textContent === "See All Users"){
+        LoadAllUsers();
+        AddUserBTN.textContent = "Hide";
+    }else{
+        AddUserBTN.textContent = "See All Users";
+        let Users = document.querySelectorAll('.RemovableUsers');
+        Users.forEach(item => {
+            item.remove();
+        })
     }
 })
