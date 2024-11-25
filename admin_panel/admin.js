e9b46ea69c7ed7f3a29248125d5c78c486af7321
sahil1607cms <sahil16072004@gmail.com>
2024-11-25 22:25:58 +0530

    cmnts

--- admin_panel/admin.js
+++ admin_panel/admin.js
@@ -3,9 +3,11 @@ BF.addEventListener("submit", function (e) {
     e.preventDefault()
 })
 
+//add book button
 let SubBtn = document.getElementById("SubmitBTN");
 SubBtn.addEventListener('click', function (e) {
     e.preventDefault();
+    //organizing data into object form
     const bookData = {
         bookName: document.getElementById('bookName').value,
         url: document.getElementById('url').value,
@@ -17,8 +19,11 @@ SubBtn.addEventListener('click', function (e) {
     };
     // Send data to PHP server using fetch
     fetch("http://localhost/ONLINEBOOKSTORE/AddBook.php", {
-        method: "POST",headers: {
-            "Content-Type": "application/json",},
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        //fetch api expects text hence converting object to text
         body: JSON.stringify(bookData),
     })
         .then(response => {
@@ -33,24 +38,26 @@ SubBtn.addEventListener('click', function (e) {
         .catch(error => {
             console.log('Error:', error);
         });
+    //the add book button disappears and loading animation appers which was earlier display hidden
     SubBtn.style.display = "none";
     const Loader = document.querySelector(".loadingBar");
     Loader.style.display = "flex";
     setTimeout(() => {
         window.location.reload();
-    },3500)
-    })
+    }, 1500)
+})
 
+//deleting the book from database where book id matches 
 function DeleteBook(BookBID) {
-    fetch("http://localhost/ONLINEBOOKSTORE/DeleteBook.php",{
-        method: "POST",headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({B_id:BookBID}),
+    fetch("http://localhost/ONLINEBOOKSTORE/DeleteBook.php", {
+        method: "POST", headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ B_id: BookBID }),
     })
-        .then(res =>{
-            if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+        .then(res => {
+            if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
             else return res.json();
         })
-        .then(data =>{
+        .then(data => {
             console.log(data.message);
         })
         .catch(error => {
@@ -58,96 +65,111 @@ function DeleteBook(BookBID) {
         })
 }
 
-function DeleteUser(UID){
-    fetch("http://localhost/ONLINEBOOKSTORE/DeleteUser.php",{
-        method: "POST",headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({uid:UID}),
+//deleting the user from database where user id matches 
+function DeleteUser(UID) {
+    fetch("http://localhost/ONLINEBOOKSTORE/DeleteUser.php", {
+        method: "POST", headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ uid: UID }),
     })
-        .then(res =>{
-            if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+        .then(res => {
+            if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
             else return res.json();
         })
-        .then(data=>{
+        .then(data => {
             console.log(data.message);
         })
         .catch(error => {
             console.log('Error:', error);
         })
-    console.log({UID});
+    console.log({ UID });
 }
 
-function LoadAllUsers(){
+function LoadAllUsers() {
     let MainContainer = document.querySelector(".UserContainer");
     fetch("http://localhost/ONLINEBOOKSTORE/GetUsers.php")
-        .then(res =>{
-            if(!res.ok) throw new Error('HTTP error! Status: ' + res.status);
+        .then(res => {
+            if (!res.ok) throw new Error('HTTP error! Status: ' + res.status);
             else return res.json();
         })
-        .then(data =>{
+        .then(data => {
             data.forEach((item) => {
+                // Create a card element for each user
                 let card = document.createElement("div");
                 card.classList.add("UserCard");
 
+                // Create and add a user icon to the card
                 let icon = document.createElement("i");
-                icon.classList.add("fa","fa-user");
+                icon.classList.add("fa", "fa-user");
                 icon.style.fontSize = "24px";
                 card.appendChild(icon);
 
+                
                 let UID = document.createElement("p");
                 UID.textContent = item.UID;
                 UID.classList.add("UserText");
                 card.appendChild(UID);
 
+                // Create and add a paragraph to display the user's name
                 let Name = document.createElement("p");
                 Name.textContent = item.Name;
                 Name.classList.add("UserText");
                 card.appendChild(Name);
 
+                // Create and add a paragraph to display the user's username
                 let UserName = document.createElement("p");
                 UserName.textContent = item.Username;
                 UserName.classList.add("UserText");
                 card.appendChild(UserName);
 
+                 // Create and add a paragraph to display the user's email
                 let Email = document.createElement("p");
                 Email.textContent = item.Email;
                 Email.classList.add("UserText");
                 card.appendChild(Email);
 
+                //create delete button and add the stlying class
                 let DeleteBTN = document.createElement("Button");
-                DeleteBTN.textContent= "Delete";
+                DeleteBTN.textContent = "Delete";
                 DeleteBTN.classList.add("DeleteBTN");
+                //on clicking it , the delete user function will be called and the button changes to deleting 
                 DeleteBTN.addEventListener("click", function (e) {
                     DeleteUser(UID.textContent)
                     DeleteBTN.textContent = "deleting..";
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         window.location.reload();
-                    }, 3500);
+                    }, 2000);
                 })
                 card.appendChild(DeleteBTN);
+                //marking this element as removeable which can be removed later 
                 card.classList.add("RemovableUsers");
                 MainContainer.appendChild(card);
             })
         })
 }
 
-let i=1;
+let i = 1;
+
+//see all book button
 let SeeAllBtn = document.getElementById("SAAB");
 let BookContainer = document.getElementById("BookContainer");
 SeeAllBtn.addEventListener("click", function (e) {
-    if(i === 0) {
+    // Check if the books are currently displayed
+    if (i === 0) {
         i++;
         SeeAllBtn.textContent = "SeeAllBooks";
         let book = document.querySelectorAll(".RemovableBook");
-        book.forEach(item => {item.remove();})
+        book.forEach(item => { item.remove(); })
         return;
     }
+
+    //the books will be displayed and the i will be flagged to 0  
     SeeAllBtn.textContent = "Hide"
     i--;
     fetch("http://localhost/ONLINEBOOKSTORE/Getbookitem.php")
-    .then(response => {
-        if(!response.ok) throw new Error('HTTP error! Status: ' + response.status);
-        else return response.json();
-    })
+        .then(response => {
+            if (!response.ok) throw new Error('HTTP error! Status: ' + response.status);
+            else return response.json();
+        })
         .then(result => {
             result.forEach((item) => {
                 let card = document.createElement("div");
@@ -164,21 +186,21 @@ SeeAllBtn.addEventListener("click", function (e) {
                 card.appendChild(BAuthor);
                 let rating = document.createElement("div");
                 rating.classList.add("rating");
-                for(let j=0; item.Rating - j >=1 ; j++){
+                for (let j = 0; item.Rating - j >= 1; j++) {
                     let star = document.createElement("i");
-                    star.classList.add("fa" ,"fa-star");
+                    star.classList.add("fa", "fa-star");
                     rating.appendChild(star);
                 }
                 card.appendChild(rating);
                 let DeleteBTN = document.createElement("Button");
-                DeleteBTN.textContent= "Delete";
+                DeleteBTN.textContent = "Delete";
                 DeleteBTN.classList.add("DeleteBTN");
                 DeleteBTN.addEventListener("click", function (e) {
                     DeleteBook(BID);
                     DeleteBTN.textContent = "deleting..";
-                    setTimeout(()=>{
+                    setTimeout(() => {
                         window.location.reload();
-                    }, 3500);
+                    }, 2000);
                 })
                 card.appendChild(DeleteBTN);
                 card.classList.add("RemovableBook");
@@ -191,13 +213,12 @@ const AddBookContainer = document.getElementById("AddBookContainer");
 AddBookContainer.style.display = "none";
 const AddBookBTN = document.getElementById("AddBook");
 AddBookBTN.addEventListener("click", function (e) {
-    if(AddBookBTN.textContent === "Add")
-    {
+    if (AddBookBTN.textContent === "Add") {
         AddBookContainer.style.display = "block";
         AddBookBTN.textContent = "Cancel";
         AddBookBTN.classList.remove("AddBTN");
         AddBookBTN.classList.add("DeleteBTN");
-    }else{
+    } else {
         AddBookContainer.style.display = "none";
         AddBookBTN.textContent = "Add";
         AddBookBTN.classList.remove("DeleteBTN");
@@ -205,12 +226,13 @@ AddBookBTN.addEventListener("click", function (e) {
     }
 })
 
+//see all users button 
 const AddUserBTN = document.getElementById("SAU");
 AddUserBTN.addEventListener("click", function (e) {
-    if(AddUserBTN.textContent === "See All Users"){
+    if (AddUserBTN.textContent === "See All Users") {
         LoadAllUsers();
         AddUserBTN.textContent = "Hide";
-    }else{
+    } else {
         AddUserBTN.textContent = "See All Users";
         let Users = document.querySelectorAll('.RemovableUsers');
         Users.forEach(item => {
